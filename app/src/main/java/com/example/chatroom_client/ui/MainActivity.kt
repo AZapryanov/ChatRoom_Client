d0ebadd63d6494425b6e78a482fddb0052e99a78
Alexandar Zapryanov <90134487+AZapryanov@users.noreply.github.com>
2022-03-24 18:08:30 +0200

    Revert "Revert "Merge branch 'Websocket-Service'""
    This reverts commit b9ed1a37a7fa163ea0262a81156b89a7a5c7f871.

--- app/src/main/java/com/example/chatroom_client/ui/MainActivity.kt
+++ app/src/main/java/com/example/chatroom_client/ui/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.chatroom_client.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.chatroom_client.data.graphql.apolloClient
 import com.example.chatroom_client.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import src.main.graphql.MessageListQuery
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,25 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(contentView)
 
         val actionbar = supportActionBar
-        actionbar!!.title = "Chat Application"
-
-        //Test GraphQL------------------------------------------------------
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val response = apolloClient.query(MessageListQuery()).execute()
-//            val getAllMessageList = response.data?.getAllMessages
-//            val messageAsData = getAllMessageList?.get(0)
-//            val message = messageAsData?.message
-//            Log.d("MessageList", "Success ${response.data}")
-//            Log.d("MessageList", "Message as data: $messageAsData")
-//            Log.d("MessageList", "Only message text: $message")
-//
-//            if (getAllMessageList != null) {
-//                for (message in getAllMessageList) {
-//                    Log.d("MessageList", message.message)
-//                }
-//            }
-//        }
-//        ------------------------------------------------------------------
+        actionbar!!.title = "User registration"
 
         binding.buttonJoinChatroom.setOnClickListener {
             val username = binding.etEnterUsername.text.toString()
@@ -52,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ChatActivity::class.java)
             intent.putExtra("username", username)
             startActivity(intent)
+            finish()
         }
     }
 }
